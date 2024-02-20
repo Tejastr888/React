4419ec10fd5240edd4f10cee74782603acd94d63
Tejas R <tejastr888@gmail.com>
2024-02-20 15:05:12 +0530

    disabled log messages

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -16,7 +16,7 @@ export const getReceiverSocketId = (recieverId) => {
 const userSocketMap = {};
 
 io.on("connection", (socket) => {
-  console.log("a user connected", socket.id);
+  // console.log("a user connected", socket.id);
   const userId = socket.handshake.query.userId;
   if (userId != undefined) userSocketMap[userId] = socket.id;
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
